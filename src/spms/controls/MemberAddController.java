6ebf4e5cdbf624b13b1e752ea8023c5cd6c6f2c8
trickstar181s2_classroom <sechanbi17@naver.com>
2016-10-27 17:35:49 +0900

    MemberAddController의 DataBinding 구현 p.483

--- src/spms/controls/MemberAddController.java
+++ src/spms/controls/MemberAddController.java
@@ -2,30 +2,41 @@ package spms.controls;
 
 import java.util.Map;
 
+import spms.bind.DataBinding;
 import spms.dao.MemberDao;
 import spms.vo.Member;
 
-public class MemberAddController implements Controller {
+public class MemberAddController implements Controller, DataBinding {
 	MemberDao memberDao;
-	
+
 	public MemberAddController setMemberDao(MemberDao memberDao) {
 		this.memberDao = memberDao;
 		return this;
 	}
 	
 	@Override
+	public Object[] getDataBinders() {
+		return new Object[] {
+				"member", spms.vo.Member.class
+		};
+	}
+	
+	@Override
 	public String execute(Map<String, Object> model) throws Exception {
-		if (model.get("member") == null) { // 입력폼을 요청할 때
+		Member member = (Member)model.get("member");
+		if (member.getEmail() == null) { // 입력폼을 요청할 때
 			return "/member/MemberForm.jsp";
 		
 		} else { // 회원 등록을 요청할 때
 			// MemberDao memberDao = (MemberDao)model.get("memberDao");
      
-			Member member = (Member)model.get("member");
+			// Member member = (Member)model.get("member");
 			memberDao.insert(member);
       
 			return "redirect:list.do";
 		}
 	}
+
+	
 }
 
