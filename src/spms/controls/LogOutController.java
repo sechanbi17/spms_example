192b7380fe035551c23b73ed757c9ba1c50ec54e
trickstar181s2_classroom <sechanbi17@naver.com>
2016-10-28 13:43:41 +0900

    Component 어노테이션 적용 - ~p.519 - Controller들, MySqlMemberDao

--- src/spms/controls/LogOutController.java
+++ src/spms/controls/LogOutController.java
@@ -4,6 +4,9 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import spms.annotation.Component;
+
+@Component("/auth/Logout.do")
 public class LogOutController implements Controller {
 
 	@Override
